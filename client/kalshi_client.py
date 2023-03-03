e006a14a91f902d2d5687cdf3004db26001024af
Lior Hirschfeld <liortulip@gmail.com>
2023-03-03 00:05:09 -0500

    Oscars best picture visualization (#1)
    * Oscars best picture visualization
    
    * Ignore pyc files

---
+++ client/kalshi_client.py
@@ -0,0 +1,24 @@
+import kalshi_python
+import yaml
+
+
+class AuthedApiInstance(kalshi_python.ApiInstance):
+    def __init__(self):
+        config = kalshi_python.Configuration()
+
+        try:
+            creds = yaml.safe_load(open("client/credentials.yaml"))
+        except FileNotFoundError:
+            raise Exception(
+                "Please create a credentials.yaml file in the client directory. See README.md for more info."
+            )
+
+        if "username" not in creds or "password" not in creds:
+            raise Exception(
+                "Please fill out the username and password fields in the credentials.yaml file."
+            )
+
+        super().__init__(configuration=config)
+
+        loginResponse = self.login(kalshi_python.LoginRequest(email=creds["username"], password=creds["password"]))
+        self.set_api_token(loginResponse.token)
